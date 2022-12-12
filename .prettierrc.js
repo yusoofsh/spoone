20ab176fa4fa7ffd5a4047a1611b7b4d6fb956e0
Yusoof SH <18055365+yusoofsh@users.noreply.github.com>
2022-12-13 00:55:31 +0700

    ➕ dep-add: integrate prettier

---
+++ .prettierrc.js
@@ -0,0 +1,10 @@
+/** @type {import('prettier').Config} */
+module.exports = {
+	printWidth: 120,
+	tabWidth: 2,
+	useTabs: true,
+	semi: false,
+	singleQuote: false,
+	trailingComma: "es5",
+	bracketSpacing: true,
+}
