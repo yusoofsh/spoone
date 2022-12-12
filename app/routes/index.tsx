ad94aa0ae84d91eeb14dd77180c1142186983fdc
Yusoof SH <18055365+yusoofsh@users.noreply.github.com>
2022-12-13 05:20:32 +0700

    ➕ dep-add: integrate tailwind

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,16 +1,3 @@
-import type { LinksFunction } from "@remix-run/node"
-
-import styles from "~/styles/globals.css"
-
-export const links: LinksFunction = () => {
-	return [
-		{
-			rel: "stylesheet",
-			href: styles,
-		},
-	]
-}
-
 export default function Index() {
 	return (
 		<>
